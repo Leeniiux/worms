5f1b0b8f0110b23d8943ce9e782060e26917c8b9
BuildTools <unconfigured@null.spigotmc.org>
2021-12-07 23:41:00 +0100

    Assets structure class MainMenu avancée

--- utils/menus/main_menu.py
+++ utils/menus/main_menu.py
@@ -7,12 +7,13 @@ from utils.states import State
 
 class MainMenu:
 
-    IMG_BACKGROUND = pygame.image.load('assets/main_menu/img_background.png')
+    IMG_BACKGROUND = pygame.image.load('assets/menus/main_menu/background.png')
     IMG_BACKGROUND_W = Config.WINDOW_W
     IMG_BACKGROUND_H = Config.WINDOW_H
     
     IMG_NEW_GAME = pygame.image.load('assets/main_menu/img_new_game.png')
     IMG_NEW_GAME_W = Config.WINDOW_W/2
+    IMG_NEW_GAME_H = Config.WINDOW_H/6
 
     @staticmethod
     def switch():
@@ -22,5 +23,4 @@ class MainMenu:
     @staticmethod
     def draw():
         Config.window.blit(MainMenu.IMG_BACKGROUND, (0, 0))
-        
-
+        Config.window.blit(MainMenu.IMG_NEW_GAME, (Config.WINDOW_W/2-MainMenu.IMG_NEW_GAME_W/2, Config.WINDOW_H/2-MainMenu.IMG_NEW_GAME_H/2))
